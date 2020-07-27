8f627b1bd5277af15bc3c4634e6f655b6272d7fd
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-27 17:51:22 -0300

    Secao 13 - Aula 158

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,18 +17,42 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   getLocation() async {
-    try {
-      Location location = Location();
-      await location.getCurrentLocation();
-      print("latitude:" + location.latitude.toString());
-      print("longitude:" + location.longitude.toString());
-    } catch (e) {
-      print(e);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print("latitude:" + location.latitude.toString());
+    print("longitude:" + location.longitude.toString());
+  }
+
+  getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+
+//    print(response.body);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      print("temperature:" + temperature.toString());
+
+      int condition = decodedData['weather'][0]['id'];
+      print("condition:" + condition.toString());
+
+      String cityName = decodedData['name'];
+      print("cityName:" + cityName.toString());
+
+      double longitude = decodedData['coord']['lon'];
+      print(longitude);
+      String weatherDescription = decodedData['weather'][0]['description'];
+      print(weatherDescription);
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
